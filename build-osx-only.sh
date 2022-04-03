26f604c2a80c56b1a543962b543675027c54437f
vertion <husby.david@protonmail.com>
2022-04-03 09:54:08 -0500

    osx: move .app and -qt from container (#2)

--- build-osx-only.sh
+++ build-osx-only.sh
@@ -6,21 +6,21 @@ rm -rf build
 
 export HOST=x86_64-apple-darwin16
 ### Place Xcode-11.3.1-11C505-extracted-SDK-with-libcxx-headers in depends/SDKs
-make -j32 -C depends HOST=$HOST
+make -j8 -C depends HOST=$HOST
 mkdir -p $WORKSPACE/out/$HOST
 ./autogen.sh
 mkdir build && cd build
 ../configure --disable-dependency-tracking --disable-tests --disable-bench --prefix=$WORKSPACE/depends/$HOST --bindir=$WORKSPACE/out/$HOST/bin --libdir=$WORKSPACE/out/$HOST/lib --disable-shared
-make -j32 install
+make -j8 install
 make deploy
 cp Vertcoin-Core.dmg $WORKSPACE/out/$HOST
+cp -r Vertcoin-Qt.app $WORKSPACE/out/$HOST
 cd ../
 rm -rf build
 rm -rf /root/output/$HOST
 mv $WORKSPACE/out/$HOST /root/output/$HOST
 
-DAEMON_BINS=("/root/output/$HOST/bin/vertcoind" "/root/output/$HOST/bin/vertcoin-tx" "/root/output/$HOST/bin/vertcoin-cli" "/root/output/$HOST/bin/vertcoin-wallet")
+DAEMON_BINS=("/root/output/$HOST/bin/vertcoind" "/root/output/$HOST/bin/vertcoin-tx" "/root/output/$HOST/bin/vertcoin-cli" "/root/output/$HOST/bin/vertcoin-wallet" "/root/output/$HOST/bin/vertcoin-qt")
 
 strip --strip-unneeded "${DAEMON_BINS[@]}"
-zip -j /root/output/vertcoind-osx-x64.zip "${DAEMON_BINS[@]}"
-cp /root/output/$HOST/Vertcoin-Core.dmg /root/output/vertcoinqt.dmg
+zip -j /root/output/vertcoin-osx.zip "${DAEMON_BINS[@]}"
